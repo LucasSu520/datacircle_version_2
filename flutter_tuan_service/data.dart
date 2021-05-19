76078f6176fc4e6b77a832f053c88c45b5f64922
LucasSu520 <suChristian0614@gmail.com>
2021-05-19 22:26:12 +0800

    finish moment page

---
+++ flutter_tuan_service/data.dart
@@ -0,0 +1,115 @@
+var TuanActivities = {
+  'recommend': [
+    {
+      'name': '学习滑板（初级）',
+      'dest': '厦门大学科艺中心',
+      'person': 10,
+      'isOfficial': true,
+      'time': "20210508",
+      'group': '运动组',
+      'imageUrl': 'images/skate.jpg'
+    },
+    {
+      'name': 'COS party',
+      'dest': '思明区Mojito',
+      'person': 50,
+      'isOfficial': true,
+      'time': "20210508",
+      'group': '青年组',
+      'imageUrl': 'images/party.jpg'
+    },
+    {
+      'name': '狼人杀(高级)',
+      'dest': '富万邦麦当劳',
+      'person': 8,
+      'isOfficial': false,
+      'time': "20210809",
+      'group': '桌游组',
+      'imageUrl': 'images/wolfKill.jpeg'
+    },
+    {
+      'name': '足球',
+      'dest': '厦门大学演武场',
+      'person': 10,
+      'isOfficial': false,
+      'time': "20210618",
+      'imageUrl': "images/football.jpg"
+    },
+  ],
+  'soon': [
+    {
+      'name': '学习滑板（初级）',
+      'dest': '厦门大学科艺教学楼下',
+      'person': 10,
+      'isOfficial': true,
+      'time': '20210508',
+      'group': '运动',
+      'imageUrl': 'images/skate.jpg',
+    },
+    {
+      'imageUrl': 'images/lunZiMa.jpg',
+      'name': 'LOL线下比赛',
+      'dest': '华侨博物馆欢乐园网咖',
+      'person': 10,
+      'time': '20210508',
+      'group': '网游',
+      'isOfficial': true
+    },
+    {
+      'imageUrl': 'images/lunZiMa.jpg',
+      'name': '美食',
+      'dest': '厦门大学南光食堂',
+      'person': 10,
+      'time': '20210508',
+      'group': '美食',
+      'isOfficial': false
+    },
+    {
+      'imageUrl': 'images/MeetingRoom.jpg',
+      'name': '电脑展示',
+      'dest': '厦门大学南强二',
+      'person': 10,
+      'time': '20210508',
+      'group': '科技',
+      'isOfficial': false
+    }
+  ],
+  'rank': [
+    {
+      'imageUrl': 'images/concert.jpg',
+      'name': '音乐会',
+      'dest': '厦门大学科艺中心',
+      'person': 10,
+      'time': '20210508',
+      'group': '音乐',
+      'isOfficial': true
+    },
+    {
+      'imageUrl': 'images/lunZiMa.jpg',
+      'name': 'LOL线下比赛',
+      'dest': '华侨博物馆欢乐园网咖',
+      'person': 10,
+      'time': '20210508',
+      'group': '网游',
+      'isOfficial': true
+    },
+    {
+      'imageUrl': 'images/lunZiMa.jpg',
+      'name': '美食',
+      'dest': '厦门大学南光食堂',
+      'person': 10,
+      'time': '20210508',
+      'group': '美食',
+      'isOfficial': false
+    },
+    {
+      'imageUrl': 'images/MeetingRoom.jpg',
+      'name': '电脑展示',
+      'dest': '厦门大学南强二',
+      'person': 10,
+      'time': '20210508',
+      'group': '科技',
+      'isOfficial': false
+    }
+  ]
+};
